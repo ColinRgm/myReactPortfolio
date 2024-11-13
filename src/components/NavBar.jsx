044074bf45331be8ff3a075f978f9be4c9718d69
Colin.rgm <colin.regamey@icloud.com>
2024-11-13 11:55:39 +0100

    feat: create the different pages and create the navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="../pages/Contact.jsx">Contact</a>
+            <a href="../pages/Parcours.jsx">Parcours</a>
+            <a href="../pages/Passion.jsx">Passions</a>
+            <a href="../pages/Project.jsx">Projects</a>
+        </nav>
+    )
+}
+
+export default NavBar;
