5212207bceeb88a309c727d40d2689df35c7ebb2
Colin.rgm <colin.regamey@icloud.com>
2024-11-18 11:24:38 +0100

    refactor: Optimize the code for Passions.jsx

---
+++ src/components/MyPassion.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function MyPassion({ title, description, url, reverse }) {
+
+    const content = {
+        display: 'grid',
+        gridTemplateColumns: reverse ? '150px auto' : "auto 150px",
+        gridTemplateRows: '250px',
+
+        width: '80%',
+
+        textAlign: 'right',
+        alignItems: 'center',
+
+        gap: "25px",
+
+        title: {
+            color: "#FC7A1E"
+        },
+
+        description: {
+            fontSize: "15px",
+        },
+
+        imgStyle: {
+            width: '150px',
+            height: '150px',
+        }
+    }
+
+    return (
+        <div style={ content }>
+            { reverse && <img src={ url } alt={ title } style={ content.imgStyle }/> }
+
+            <div style={ {
+                borderLeft: !reverse ? "none" : "#FC7A1E solid 3px",
+                borderRight: !reverse ? "#FC7A1E solid 3px" : "none",
+                paddingLeft: !reverse ? "0" : "25px",
+                paddingRight: !reverse ? "25px" : "0"
+            } }>
+                <h3 style={ content.title }>{ title }</h3>
+                <p style={ content.description }>{ description }</p>
+            </div>
+
+            { !reverse && <img src={ url } alt={ title } style={ content.imgStyle }/> }
+
+        </div>
+    )
+}
+
+export default MyPassion;
